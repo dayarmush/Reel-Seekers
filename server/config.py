37cbf958f5cc0566e55ef47b202028e1fdf3e019
Dovid Yarmush <yarmushda@gmail.com>
2023-07-24 12:37:29 -0400

    create user model

--- server/config.py
+++ server/config.py
@@ -4,5 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_bcrypt import Bcrypt
 from sqlalchemy_serializer import SerializerMixin as SM
+from sqlalchemy.exc import IntegrityError
 
 db = SQLAlchemy()
---
+++ server/models/user.py
@@ -0,0 +1,36 @@
+from config import db, Bcrypt, SM
+from sqlalchemy.ext.hybrid import hybrid_property
+
+bcrypt = Bcrypt()
+
+class User(db.Model, SM):
+    __tablename__ = 'Users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    _password_hash = db.Column(db.String, nullable=False)
+    Admin = db.Column(db.Boolean, default=False)
+    points = db.Column(db.Integer, default=0)
+
+    favorites = db.relationship('Favorite', backref='user')
+    edits = db.relationship('Edit', backref='user')
+    reviews = db.relationship('Review', backref='user')
+    messages = db.relationship('Message', backref='user')
+
+    serialize_rules = ('-favorites.user', '-edits.user', '-reviews.user', '-messages.user')
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('Cannot access password hash')
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        hashed_password = bcrypt.generate_password_hash(password.encode('utf-8'))
+        self._password_hash = hashed_password.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
+    
+    def __repr__(self):
+        return f'(User: {self.username})'
+
